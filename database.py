949fe64a9bee68b7637ecced2c0e24638be253c3
shadanxd <shadan@oneassure.in>
2022-11-15 16:37:01 +0530

    reduced crud operations removed global variables justified class methods

--- database.py
+++ database.py
@@ -1,4 +1,3 @@
-#new line
 from pymongo import MongoClient
 from dotenv import load_dotenv
 import os
@@ -6,36 +5,29 @@ import os
 load_dotenv()
 db_user = os.environ.get('username')
 db_password = os.environ.get('password')
-cluster = MongoClient(f'mongodb+srv://{db_user}:{db_password}@cluster0.ef68wuq.mongodb.net/?retryWrites=true&w=majority')
-db = cluster["Cluster0"]
-collections = db["OneAssure"]
 
 
 class DBHandler:
-    def __init__(self, username: str):
-        self.username = username
+
+    cluster = MongoClient(f'mongodb+srv://{db_user}:{db_password}@cluster0.ef68wuq.mongodb.net/?retryWrites=true&w'
+                          f'=majority')
+    db = cluster["Cluster0"]
+    collections = db["OneAssure"]
 
     @classmethod
     async def save(cls, item: dict):
-        collections.insert_one(item)
-        return cls(item['_id'])
+        cls.collections.insert_one(item)
 
-    async def user_query(self):
-        results = collections.find_one({"_id": self.username})
-        if results:
-            return results['password']
-        else:
+    @classmethod
+    async def update_user_details(cls, scope: dict):
+        if 'new_name' in scope:
+            cls.collections.update_one({"username": scope['username']}, {"$set": {"name": scope['new_name']}})
+        if 'new_number' in scope:
+            cls.collections.update_one({"username": scope['username']}, {"$set": {"phone": scope['new_number']}})
+
+    @classmethod
+    async def getUserDetails(cls, username: str):
+        user = cls.collections.find_one({"username": username},  {"_id": 0})
+        if user is None:
             return None
-
-    async def update_phone(self, phone: int):
-        collections.update_one({"_id": self.username}, {"$set": {"phone": phone}})
-
-    async def name_update(self, new_name: str):
-        collections.update_one({"_id": self.username}, {"$set": {"name": new_name}})
-
-    async def getUserDetails(self):
-        user = collections.find_one({"_id": self.username})
         return user
-
-
-
--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import random
-import string
 from typing import Optional
 import uvicorn
 from fastapi import Depends
@@ -22,34 +20,29 @@ class User(BaseModel):
 
 @app.put('/signup/')
 async def signup(user: User):
-    item = {"_id": user.username, "name": user.name, "phone": user.phone,
-            "password": AuthHandler.get_password_hash(user.password), "description": user.description,
-            "Key": generate_random()}
+    item = {"username": user.username, "name": user.name, "phone": user.phone,
+            "password": AuthHandler.get_password_hash(user.password), "description": user.description}
     await DBHandler.save(item)
     return user
-
-
-def generate_random():
-    x = ''.join(random.choice(string.ascii_uppercase + string.ascii_lowercase + string.digits) for _ in range(6))
-    return x
 
 
 @app.post('/login')
 async def login(form_data: OAuth2PasswordRequestForm = Depends()):
     username = form_data.username
     password = form_data.password
-    user = DBHandler(username)
-    hashed_password = await user.user_query()
-    if hashed_password and AuthHandler.verify_password(password, hashed_password):
-        return AuthHandler.encode_token(username)
-    else:
-        return {"Password/Username": "Incorrect"}
+    user = await DBHandler.getUserDetails(username)
+    if user is None:
+        return {"username/password": "Invalid"}
+    elif username == user['username'] and AuthHandler.verify_password(password, user['password']):
+        return {AuthHandler.encode_token(username)}
 
 
 @app.post('/login/update/phone/{username}/{number}')
 async def update(username: str, number: int, payload: dict = Depends(AuthHandler.decode_token)):
     if payload['sub'] == username:
-        await Data(payload['sub']).update_phone(number)
+        scope = {"username": username, "new_number": number}
+        await DBHandler.update_user_details(scope)
+        return {"status": "phone number updated"}
     else:
         return HTTPException(status_code = 401, detail = 'Invalid Token')
 
@@ -57,7 +50,9 @@ async def update(username: str, number: int, payload: dict = Depends(AuthHandler
 @app.post('/login/update/name/{username}/{new_name}')
 async def name_update(username: str, new_name: str, payload: dict = Depends(AuthHandler.decode_token)):
     if payload['sub'] == username:
-        await Data(payload['sub']).name_update(new_name)
+        scope = {"username": username, "new_name": new_name}
+        await DBHandler.update_user_details(scope)
+        return {"status": "name updated"}
     else:
         return HTTPException(status_code = 401, detail = 'Invalid Token')
 
@@ -65,7 +60,8 @@ async def name_update(username: str, new_name: str, payload: dict = Depends(Auth
 @app.get('/login/getDetails/{username}')
 async def getDetails(username: str, payload: dict = Depends(AuthHandler.decode_token)):
     if payload['sub'] == username:
-        return await DBHandler(payload['sub']).getUserDetails()
+        user = await DBHandler.getUserDetails(username)
+        return user
     else:
         return HTTPException(status_code = 401, detail = 'Invalid Token')
 
